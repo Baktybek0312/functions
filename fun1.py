62d6856030a39039d9c7815c6088becd6f373a60
Baha Bro <toktobekov1995@gmail.com>
2021-05-07 17:35:01 +0600

    перезалил, а старое удалил

---
+++ fun1.py
@@ -0,0 +1,19 @@
+num = [1, '2', 3, '4', 5, '6a', 7, '8']
+
+# try:
+#     for i in range(-5,5):
+#         if int(I) % 2 == 0:
+#             print(int(num[7])/i)
+# except SyntaxError:
+#     print("Систактическая ошибка")
+# except NameError:
+#     print("Ошибка имен")
+# except IndexError:
+#     print("Ошибка индексации")
+# else:
+#     print("Что-то еще!")
+# finally:
+#     print("В любом случае")
+from random import randrange, random
+
+print(random.randrange(3, 10))
